b6837268c937aa9c1b46e91d21fc836c44d201fe
Fidel <fhgalano@gmail.com>
2022-05-08 21:50:41 -0400

    Added a cache for storing links that have already been visited. Fixed filtering to include all wiki links and local wiki extensions

--- scraper.py
+++ scraper.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+url_cache = dict()
+all_emails = dict()
+
 
 def rec_scraper(url, max_depth=2):
     # get html info
@@ -9,14 +12,26 @@ def rec_scraper(url, max_depth=2):
 
     # check status
     if page.status_code != 200:
-        exit()
+        url_cache[url] = None
+        print(max_depth, url)
+        return
 
     # make the soup
-    data = BeautifulSoup(page.text)
+    data = BeautifulSoup(page.text, features="html.parser")
 
     # search for links to other wiki pages
     possible_links = data.find_all('a')
-    links = [link for link in possible_links if 'href' in str(link) and 'wiki.pumping' in str(link)]
+    links = [link for link in possible_links if 'href' in str(link)]
+
+    wiki_links = []
+    for link in possible_links:
+        if link.get('href'):
+            is_wiki_link = 'wiki.pumpingstationone' in link['href']
+            is_wiki_extension = link['href'][0] == '/'
+            if is_wiki_extension:
+                wiki_links.append('https://wiki.pumpingstationone.org' + link['href'])
+            elif is_wiki_link:
+                wiki_links.append(link['href'])
 
     # search for emails
     emails = []
@@ -24,20 +39,29 @@ def rec_scraper(url, max_depth=2):
     if search:
         [emails.append(email) for email in search]
 
+    # cache url and the emails
+    if not url_cache.get(url):
+        url_cache[url] = emails
+        if emails:
+            all_emails[url] = emails
+
     # recursively search if not at depth
     if max_depth > 0:
         max_depth -= 1
-        for link in links:
-            rec_scraper(link['href'], max_depth=max_depth)
+        for link in wiki_links:
+            if link not in url_cache:
+                rec_scraper(link, max_depth=max_depth)
 
-    print(emails)
+    # print(emails)
     return emails
 
 
 if __name__ == '__main__':
     # get starting page (ps1 main page)
-    # starting_page = 'https://wiki.pumpingstationone.org/'
-    starting_page = 'https://wiki.pumpingstationone.org/Category:Wood_Shop'
+    starting_page = 'https://wiki.pumpingstationone.org/'
+    # starting_page = 'https://wiki.pumpingstationone.org/Category:Wood_Shop'
 
-    rec_scraper(starting_page)
-    print('debug')
+    rec_scraper(starting_page, max_depth=2)
+    for emails in all_emails.items():
+        for email in emails:
+            print(email)
